bd9ffabe43da5445c606383a04ec0c05acf8eab9
ninog <ninny21051992@gmail.com>
2023-02-17 11:03:18 +0200

    writing in Excel - actors ex

--- src/com/selenium/excel.java
+++ src/com/selenium/excel.java
@@ -11,37 +11,60 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.*;
-import java.security.Key;
-
-//import static jdk.internal.org.jline.utils.InfoCmp.Capability.tab;
 
 public class excel {
-    public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("C:\\Users\\ninog\\Downloads\\excel_test.xlsx");
-        String path = "C:\\Users\\ninog\\Downloads\\excel_test.xlsx";
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        XSSFSheet sheet = wb.getSheet("test");
-        int FirstRow = sheet.getFirstRowNum();
-        int LastRow = sheet.getLastRowNum();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://google.com");
 
-        for (int i = FirstRow; i <= LastRow; i++) {
-            XSSFRow row_r = sheet.getRow(i+1);
-            XSSFCell cell_r = row_r.getCell(0);
-            String value = cell_r.getStringCellValue();
-            //System.out.println(value);
-            driver.findElement(By.className("gLFyf")).sendKeys(value + Keys.ENTER);
-            String cell_value = driver.findElement(By.xpath("//*[@id='kp-wp-tab-overview']/div[1]/div/div/div/div/div/div[2]/div/span/text()")).getText();
-            excelPrint(path,cell_value,i,1);
+    static String path = "C:\\Users\\ninog\\Downloads\\excel_test.xlsx";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        try {
+
+            FileInputStream fis = new FileInputStream(path);
+            XSSFWorkbook wb = new XSSFWorkbook(fis);
+            XSSFSheet sheet = wb.getSheet("test");
+            int FirstRow = sheet.getFirstRowNum();
+            int LastRow = sheet.getLastRowNum();
+
+            String value;
+            String cell_value;
+
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.get("https://google.com");
+
+            for (int i = FirstRow + 1; i <= LastRow; i++) {
+                try {
+                    XSSFRow row_r = sheet.getRow(i);
+                    XSSFCell cell_r = row_r.getCell(0);
+                    value = cell_r.getStringCellValue();
+                    Thread.sleep(500);
+                    driver.findElement(By.name("q")).clear();
+                    Thread.sleep(1000);
+                    driver.findElement(By.name("q")).sendKeys(value + Keys.ENTER);
+                    Thread.sleep(1000);
+
+                    cell_value = driver.findElement(By.xpath("(//div[@class='VwiC3b yXK7lf MUxGbd yDYNvb lyLwlc lEBKkf'])[1]")).getText();
+                    excelPrint(path, cell_value, 0, i, 1);
+
+                } catch (Exception e) {
+                    System.out.println("Failed to read/write Excel!");
+                }
+            }
+
+            Thread.sleep(500);
+            driver.quit();
+
+        } catch(Exception e){
+            System.out.println("Failed to run the exercise!");
         }
     }
 
-    public static void excelPrint(String filePath, String value, int rowNumber, int cellNumber) throws IOException {
-        FileInputStream inputStream = new FileInputStream(new File(filePath));
+
+    public static void excelPrint(String filePath, String value, int sheetNumber, int rowNumber, int cellNumber) throws IOException {
+        FileInputStream inputStream = new FileInputStream(filePath);
         Workbook workbook = WorkbookFactory.create(inputStream);
-        Sheet sheet = workbook.createSheet("Java Books");
+        Sheet sheet = workbook.getSheetAt(sheetNumber);
         Row row = sheet.getRow(rowNumber);
         if (row == null) {
             row = sheet.createRow(rowNumber);
@@ -50,16 +73,11 @@ public class excel {
         if (cell == null) {
             cell = row.createCell(cellNumber);
         }
-        //cell.setCellValue(value);
+        cell.setCellValue(value);
         FileOutputStream outputStream = new FileOutputStream(filePath);
         workbook.write(outputStream);
         workbook.close();
         outputStream.close();
     }
 
-
 }
-
-
-// comment
-// another comment
