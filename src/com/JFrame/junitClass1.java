6577de8eb2c6c9497ce150973f76f24b45a398b1
ninog <ninny21051992@gmail.com>
2023-04-02 20:02:18 +0300

    import changes

--- src/com/JFrame/junitClass1.java
+++ src/com/JFrame/junitClass1.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 public class junitClass1 {
     static WebDriver driver;
     static String alert1;
